b804f27e1a13a2ddc84671a53dcea7435d9f3fad
Sai Suresh <saisure18@gmail.com>
2024-10-05 14:01:58 +0530

    SAI Lab_001

---
+++ src/ex_03102024/Lab_002.java
@@ -0,0 +1,20 @@
+package ex_03102024;
+
+
+public class Lab_002 {
+    public static void main(String[] args) {
+
+        System.out.println("10*1=10");
+        System.out.println("10*2=20");
+        System.out.println("10*3=30");
+        System.out.println("10*4=40");
+        System.out.println("10*5=50");
+        System.out.println("10*6=70");
+        System.out.println("10*8=80");
+        System.out.println("10*9=90");
+        System.out.println("10*10=100");
+        System.out.println("10*11=110");
+        System.out.println("10*12=120");
+
+    }
+}
