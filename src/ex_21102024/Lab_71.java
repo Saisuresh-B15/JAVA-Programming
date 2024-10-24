e4fc9df4e54e9fa178b509de28465b25202b8c10
Sai Suresh <saisure18@gmail.com>
2024-10-24 18:52:37 +0530

    ex_21102024

---
+++ src/ex_21102024/Lab_71.java
@@ -0,0 +1,14 @@
+package ex_21102024;
+
+public class Lab_71 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 13;
+
+        if(a==b){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
